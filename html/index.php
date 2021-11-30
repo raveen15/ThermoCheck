dfc2cd598f44ecf238b1dfed5e960b3d05349027
Owais <owais.quadri@ontariotechu.net>
2021-11-29 22:01:37 -0500

    300

--- html/index.php
+++ html/index.php
@@ -20,23 +20,6 @@
         <!-- Navbar Brand-->
         <a class="navbar-brand" href="index.html">ThermoCheck</a>
     </nav>
-    <div id="layoutSidenav">
-        <div id="layoutSidenav_nav">
-            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                <div class="sb-sidenav-menu">
-                    <div class="nav">
-                        <a class="nav-link" href="index.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Dashboard
-                        </a>
-                        <a class="nav-link" href="charts.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                            Charts
-                        </a>
-                    </div>
-                </div>
-            </nav>
-        </div>
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
--- html/js/scripts.js
+++ html/js/scripts.js
@@ -25,7 +25,63 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 setTempAndHumid()
 setInterval(function () { setTempAndHumid() }, 1000);
+/*
 
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 40000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+*/
 //https://ros-temphumid.herokuapp.com/history/?format=json
 async function setTempAndHumid() {
     //get temp and humid from api
@@ -40,10 +96,10 @@ async function setTempAndHumid() {
     var currentHumid = response["humidity"];
     console.log("tick");
     //change color for bad temp/humid
-    if (currentTemperature < 25) {
+    if (currentTemperature < 250) {
         //less than 250 degrees
         document.getElementById("tempCard").classList.add("bg-success")
-    } else if (currentTemperature >= 25 && currentTemperature < 30) {
+    } else if (currentTemperature >= 250 && currentTemperature < 300) {
         //greater than 250 degrees but less than 300
         document.getElementById("tempCard").classList.add("bg-warning")
     } else {
@@ -63,5 +119,7 @@ async function setTempAndHumid() {
     //set value to currtemp card
     document.getElementById("currentTemperature").innerHTML = currentTemperature + "°C"
     document.getElementById("currentHumidity").innerHTML = currentHumid + "%"
+
+
 }
 
