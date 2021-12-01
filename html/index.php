a81a4740f3313bbe8a3517139f37096d29b3fc1e
raveen15 <raveenth15@gmail.com>
2021-12-01 03:49:19 +0000

    Updated Table

--- html/index.php
+++ html/index.php
@@ -32,7 +32,7 @@
         <footer class="py-4 bg-light mt-auto">
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy; ROS Enterprises 2021</div>
+                    <div class="text-muted">Copyright &copy; ROS Enterprise 2021</div>
                 </div>
             </div>
         </footer>
@@ -44,7 +44,7 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-    <script src="js/datatables-simple-demo.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
